fae91997c2bc588f7888e699415c89fe4833896c
Jossivon <josselyn.barahona@espoch.edu.ec>
2019-01-25 19:15:16 -0500

    win

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FEderacion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FEderacion
+            // 
+            this.FEderacion.AutoSize = true;
+            this.FEderacion.Location = new System.Drawing.Point(190, 101);
+            this.FEderacion.Name = "FEderacion";
+            this.FEderacion.Size = new System.Drawing.Size(57, 13);
+            this.FEderacion.TabIndex = 0;
+            this.FEderacion.Text = "federacion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FEderacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FEderacion;
+    }
+}
+
